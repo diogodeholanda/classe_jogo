a210f701bd701e74b981282a726c16a2c9bc4e95
diogodeholanda <diogodeholanda@hotmail.com>
2024-01-21 13:14:26 -0300

    ultimo comnit

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
 class heroi
 {
-   construtor(nome,idade,tipo,ataque)
+   constructor(nome, idade, tipo, ataque)
    {
       this.nome = nome
       this.idade = idade
       this.tipo = tipo
       this.ataque = ataque
    }
-
-   let heroijogo = new heroi ("Rocket","20","ninja","")
-
-   escrever()
-   {
-      console.log(`O ${this.tipo} atacou usando ${this.ataque}`)
-   }
    
    atacar()
    {
       if (this.tipo == "mago")
       {
-          this.ataque = "magia"
+         this.ataque = "magia"
       }
       else if (this.tipo == "guerreiro")
       {
@@ -31,68 +24,23 @@ class heroi
       }
       else if (this.tipo == "ninja")
       {
-          this.ataque = "shuriken"
+         this.ataque = "shuriken"
       }
-      return this.ataque
+      console.log(`atacou usando ${this.ataque}`)
    }
+   
+   escrever()
+   {
+      console.log(`O Herói do tipo ${this.tipo}`)
+   }
+}   
 
-}
-
-
-
-
-
-
+let heroidojogo = new heroi ("Rocket",20,"ninja",)
 
 let contador = 0
 
-let resultado = rankearSaldo(100,10)
-let nivelFinal = rankearNivel(resultado)
-
-function rankearSaldo(vitorias,derrotas)
-{
-   let saldo = vitorias-derrotas
-   return saldo
-}
-
-function rankearNivel(rankearSaldo)
-{
-   let saldo = rankearSaldo
-   let nivel = ""
-
-   if (saldo <10)
-   {
-       nivel = "Ferro"
-   }
-   else if (saldo>10 && saldo<21)
-   {
-      nivel = "Bronze"
-   }
-   else if (saldo>20 && saldo<51)
-   {
-      nivel = "Prata"
-   }
-   else if (saldo>50 && saldo<81)
-   {
-      nivel = "Ouro"
-   }
-   else if (saldo>80 && saldo<91)
-   {
-      nivel = "Diamante"
-   }
-   else if (saldo>90 && saldo<101)
-   {
-      nivel = "Lendário"
-   }
-   else if (saldo>100)
-   {
-      nivel = "Imortal"
-   }
-   
-   return nivel
-}
-   
 do{
-   console.log("O Herói tem saldo de " + resultado +" está no nível de " +nivelFinal)
+   heroidojogo.escrever()
+   heroidojogo.atacar()
    contador++
 } while (contador <1)
